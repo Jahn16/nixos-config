9ad917abab7b11453a11c1b23fa1481819b58bd4
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-12 21:45:30 -0300

    feat: add nixvim as input

--- flake.nix
+++ flake.nix
@@ -8,6 +8,9 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -35,7 +38,7 @@
       homeConfigurations = {
         "jahn@nixos" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
+          extraSpecialArgs = { inherit inputs; };
           # > Our main home-manager configuration file <
           modules = [ ./home-manager/home.nix ];
         };
