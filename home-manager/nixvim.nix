0437f032fa1f235b3b2374b23631d213f24102f5
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-07-17 20:14:53 -0300

    feat: add tab and s-tab navigation in mini completion

--- home-manager/nixvim.nix
+++ home-manager/nixvim.nix
@@ -41,6 +41,13 @@
       }
 
     ];
+    extraConfigLua = ''
+      local imap_expr = function(lhs, rhs)
+        vim.keymap.set('i', lhs, rhs, { expr = true })
+      end
+      imap_expr('<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+      imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+    '';
     plugins = {
       tmux-navigator.enable = true;
       nvim-tree.enable = true;
