4907cc48a8331cd52c788a32d4f9c323dde7c8be
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-03 13:42:28 -0300

    fix: simplify startship config

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -5,22 +5,17 @@
     enableTransience = true;
     settings = {
       add_newline = true;
-      os = {
-        disabled = false;
-        symbols = {
-          NixOS = " ";
-          Arch = " ";
-        };
-      };
-      directory = {
-        format = "[  $path]($style)[$read_only]($read_only_style)";
-      };
       git_branch = {
         symbol = " ";
       };
       python.symbol = " ";
       golang.symbol = " ";
-      format = "$os $directory $git_branch$git_status$python$golang$nodejs\n$character";
+      cmd_duration.disabled = true;
+      nix_shell.disabled = true;
+      kubernetes = {
+        disabled = false;
+        detect_folders = [ "charts/" "templates/" ];
+      };
     };
   };
 }
