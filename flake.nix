7e79143c083ef591787a814b2f8c96ca0666f8fd
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-08-10 09:44:34 -0300

    fix: remove nixvim follow

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    # nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
