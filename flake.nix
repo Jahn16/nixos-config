da1cc953720e47e2188a8facb163cd663cd66653
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-03 19:39:16 -0300

    feat: add desktop module

--- flake.nix
+++ flake.nix
@@ -12,21 +12,34 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/laptop
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.jahn = ./home/home.nix;
-          home-manager.extraSpecialArgs.inputs = inputs;
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
 
-      ];
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/desktop
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jahn = ./home/home.nix;
+            home-manager.extraSpecialArgs.inputs = inputs;
+          }
+        ];
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/laptop
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jahn = ./home/home.nix;
+            home-manager.extraSpecialArgs.inputs = inputs;
+          }
+        ];
+      };
     };
   };
 }
