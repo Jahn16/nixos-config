4e265f45114f5ec0de38bc74cc250801872e899d
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-04 10:10:14 -0300

    fix: rename deprecated option

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -65,8 +65,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -121,8 +121,6 @@
     git
     vim
     wget
-    gnomeExtensions.paperwm
-    gnomeExtensions.tiling-shell
     gnomeExtensions.forge
   ];
   environment.variables.EDITOR = "vim";
