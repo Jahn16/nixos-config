471985b4c3cbadde188ebde8c02999a141ee3842
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-05 13:24:55 -0300

    fix: replace gnome for hyprland

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -45,12 +45,7 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  programs.hyprland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
