d3825f552f063ee58c1362fa64d70a7d0e8031cb
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-05 18:59:43 -0300

    feat: support for logitech receiver

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -131,4 +131,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+
+  # Logitech Unifying Receiver
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
 }
