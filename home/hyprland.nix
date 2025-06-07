2f312cc58746370e0c460fa75c5cae9a888e18a3
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-07 12:05:54 -0300

    fix: limit volume to 100%

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -46,7 +46,7 @@
           # Audio Control
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-          ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
           # Media Control
           ", XF86AudioPlay, exec, playerctl play-pause"
           ", XF86AudioPause, exec, playerctl play-pause"
